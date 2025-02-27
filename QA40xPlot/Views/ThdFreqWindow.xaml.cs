f54879d7fadd4497ab7e8c7ff1439478249f8106
Mark Zachmann <MZachmann@users.noreply.github.com>
2025-02-27 12:14:17 -0500

    Add project files.

---
+++ QA40xPlot/Views/ThdFreqWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QA40xPlot
+{
+	/// <summary>
+	/// Interaction logic for ThdFreqWindow.xaml
+	/// </summary>
+	public partial class ThdFreqWindow : Window
+	{
+		public ThdFreqWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
